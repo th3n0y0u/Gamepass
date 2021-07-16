1effdede894d7f5795a1448b56f2caff82090538
soop <77305497+th3n0y0u@users.noreply.github.com>
2021-07-16 15:34:11 -0700

    Update localscript.lua

--- localscript.lua
+++ localscript.lua
@@ -2,15 +2,9 @@ local MarketplaceService = game:GetService("MarketplaceService")
 
 local gamepassId = 17684371
 
-MarketplaceService.PromptGamePassPurchaseFinished:Connect(function(player, purchasePassID, purchaseSuccess)
-
-	if purchaseSuccess == true and purchasePassID == gamepassId then
-		print("bought gamepass")
-		player:Kick("Congrats! You have been banned!")
-	end
-end)
-
-game.Players.PlayerAdded:Connect(function(player)
+local function promptPurchase()
+	
+	local player = game.Players.LocalPlayer
 	local hasPass = false
 	
 	local success, message = pcall(function()
@@ -18,7 +12,11 @@ game.Players.PlayerAdded:Connect(function(player)
 	end)
 	
 	if hasPass == true then
-		print("bought gamepass")
-		player:Kick("I thought you wanted to ban yourself? Note: Please delete the gamepass if you want to unban yourself.")
+		print("Player has gamepass")
+	else
+		MarketplaceService:PromptGamePassPurchase(player, gamepassId)
 	end
-end)
+end
+
+wait(1)
+script.Parent.MouseButton1Down:Connect(promptPurchase)
